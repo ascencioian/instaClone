857418e549879c478490dcb61e10e59f9f0a2ddc
Ian Ascencio <88950285+ascencioian@users.noreply.github.com>
2021-11-21 16:29:24 -0500

    Delete config directory

--- config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'url' : 'mongodb+srv://admin:HT4bS37vGpS8LyL@cluster1.yi7wc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
-    'dbName': 'instagram'
-};
